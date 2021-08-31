e4b3f2c02ad9644cbd8432d46730c1909a7e07b6
Ronald van Woensel <rvw@cosninix.com>
2021-09-01 01:15:09 +0200

    incrementally

--- src/LaravelTimezoneServiceProvider.php
+++ src/LaravelTimezoneServiceProvider.php
@@ -25,41 +25,41 @@ class LaravelTimezoneServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Allow migrations publish
-        if (! class_exists('AddTimezoneColumnToUsersTable')) {
-            $this->publishes([
-                __DIR__ . '/database/migrations/add_timezone_column_to_users_table.php.stub' => database_path('/migrations/' . date('Y_m_d_His') . '_add_timezone_column_to_users_table.php'),
-            ], 'migrations');
-        }
-
-        // Register the Timezone alias
-        AliasLoader::getInstance()->alias('Timezone', \JamesMills\LaravelTimezone\Facades\Timezone::class);
-
-        // Register an event listener
-        $this->registerEventListener();
-
-        // Allow config publish
-        $this->publishes([
-            __DIR__ . '/config/timezone.php' => config_path('timezone.php'),
-        ], 'config');
-
-        // Register a blade directive to show user date/time in their timezone
-        Blade::directive(
-            'displayDate',
-            function ($expression) {
-                $options = explode(',', $expression);
-
-                if (count($options) == 1) {
-                    return "<?php echo e(Timezone::convertToLocal($options[0])); ?>";
-                } elseif (count($options) == 2) {
-                    return "<?php echo e(Timezone::convertToLocal($options[0], $options[1])); ?>";
-                } elseif (count($options) == 3) {
-                    return "<?php echo e(Timezone::convertToLocal($options[0], $options[1], $options[2])); ?>";
-                } else {
-                    return 'error';
-                }
-            }
-        );
+//        // Allow migrations publish
+//        if (! class_exists('AddTimezoneColumnToUsersTable')) {
+//            $this->publishes([
+//                __DIR__ . '/database/migrations/add_timezone_column_to_users_table.php.stub' => database_path('/migrations/' . date('Y_m_d_His') . '_add_timezone_column_to_users_table.php'),
+//            ], 'migrations');
+//        }
+//
+//        // Register the Timezone alias
+//        AliasLoader::getInstance()->alias('Timezone', \JamesMills\LaravelTimezone\Facades\Timezone::class);
+//
+//        // Register an event listener
+//        $this->registerEventListener();
+//
+//        // Allow config publish
+//        $this->publishes([
+//            __DIR__ . '/config/timezone.php' => config_path('timezone.php'),
+//        ], 'config');
+//
+//        // Register a blade directive to show user date/time in their timezone
+//        Blade::directive(
+//            'displayDate',
+//            function ($expression) {
+//                $options = explode(',', $expression);
+//
+//                if (count($options) == 1) {
+/*                    return "<?php echo e(Timezone::convertToLocal($options[0])); ?>";*/
+//                } elseif (count($options) == 2) {
+/*                    return "<?php echo e(Timezone::convertToLocal($options[0], $options[1])); ?>";*/
+//                } elseif (count($options) == 3) {
+/*                    return "<?php echo e(Timezone::convertToLocal($options[0], $options[1], $options[2])); ?>";*/
+//                } else {
+//                    return 'error';
+//                }
+//            }
+//        );
     }
 
     /**
